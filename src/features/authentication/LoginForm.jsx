ec62d7cd14ea1a01447d3b5f18964a5056b58ef4
kahalagan00 <privatelampas@gmail.com>
2024-11-17 13:51:13 -0800

    Added more functionality for user Login and Sign Up

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -17,7 +17,15 @@ function LoginForm() {
       return;
     }
 
-    login({ email, password });
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
   }
 
   return (
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,24 @@
 import supabase from "./supabase";
 
+export async function signup({ full_name, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        full_name,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -11,4 +30,29 @@ export async function login({ email, password }) {
   }
 
   return data;
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) {
+    return null;
+  }
+
+  const { data, error } = await supabase.auth.getUser();
+
+  console.log(data);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    throw new Error(error.message);
+  }
 }
